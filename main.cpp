bdc3ae9b9dcb0bc6243ca6dc080ca95cb394d5e4
naseem umar masaid <naseem0shalahuddin@gmail.com>
2022-09-24 15:04:49 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,91 +1,115 @@
 #include<iostream>
-    #define max 1000
+#include<conio.h>
 
-    using namespace std;
 
-    class sstack{
-        int a[max];
-        int top;
+using namespace std;
+struct paket
+{
+    char jenis_brg[30];
+    int harga,berat,totalHarga;
+    char kota[30];
+};
 
-        public:
-            void initialize();
-            void push(int val);
-            void pop();
-            void peep();
-            void display();
-    };
+struct paket kirim[100];
 
-    void sstack::initialize(){
-        cout<<"stack kosong telah di buat ";
-        top=-1;
-    }
+/*******************************************/
 
-    void sstack::push(int val){
-        if(top==max-1){
-            cout<<"\nStack sudah penuh...";
-            cout<<"\nStack sudah penuh...";
-        }
-        a[++top]=val;
-    }
 
-    void sstack::pop(){
-        if(top==-1){
-            cout<<"\nStack Berhasil di kosongkan..";
-        }
-        else{
-            cout<<"\nPopnya adalah :"<<a[top];
-            top--;
-        }
-    }
+void tambahdata(int x)
+{
+    cout<<"Inputkan Jenis Barang    :";
+    cin>>kirim[x].jenis_brg;
+    cout<<"Berat Barang             :";
+    cin>>kirim[x].berat;
+    cout<<"Inputkan Harga           :";
+    cin>>kirim[x].harga;
+    cout<<"Inputkan Kota Tujuan     :";
+    cin>>kirim[x].kota;
+}
 
-    void sstack::peep(){
-        if(top==-1){
-            cout<<"\nstack berhasil di kosongkan..";
-        }
-        else{
-            cout<<"\nThe Topmost element is.."<<a[top];
-        }
-    }
-
-    void sstack::display(){
-        if(top==-1){
-            cout<<"\nStack kosong..";
-        }
-        else{
-            cout<<"\nStacknya adalah..";
-            int x=top;
-            while(x!=-1){
-                cout<<a[x]<<" ";
-                x--;
-            }
-        }
-    }
-
-    int main()
+void tampildata(int x)
+{
+    cout<<"Daftar Kiriman Paket"<<endl;
+    for(int a=1; a<x; a++)
     {
-        sstack s;
-        int ch,x,val;
-        s.initialize();
-        cout<<"\nPilihan : \n1) Push \n2) Pop \n3) Peep \n4) Display \n5) Exit :P";
-        while(1){
-            cout<<"\nmasukan pilihan..";
-            cin>>ch;
-            if(ch==1){
-                cout<<"\nMasukan Data..";
-                cin>>val;
-                s.push(val);
-            }
-            else if(ch==2){
-               s.pop();
-            }
-            else if(ch==3){
-                s.peep();
-            }
-            else if(ch==4){
-                s.display();
-            }
-            else break;
-        }
-        return 0;
+        cout<<a<<" ";
+        cout<<kirim[a].jenis_brg<<" ";
+        cout<<kirim[a].berat<<" ";
+        cout<<kirim[a].harga<<" ";
+        cout<<kirim[a].kota<<endl;
     }
+    cout<<"Press Any Key...";
+    getch();
+    /*harus Ijin --> #include<conio.h>*/
+}
 
+void hapusdata(int x)
+{
+    cout<<"Data Terakhir yang anda Hapus Adalah :"<<endl;
+    cout<<kirim[x].jenis_brg<<" ";
+    cout<<kirim[x].berat<<" Kg";
+    cout<<kirim[x].harga<<" Rp ";
+    cout<<kirim[x].kota<<endl<<endl;
+
+    cout<<"Press Any Key...";
+    getch();
+
+
+}
+
+void totalHarga(int x)
+{   int s=0;
+   for(int i=0;i<x;i++)
+    {s=s+kirim[i].harga;}
+    cout << "Total Berat angkutan yang di bawa adalah " << s << " Rp" << endl;;
+}
+
+
+void totalberat(int x)
+{   int w=0;
+   for(int i=0;i<x;i++)
+    {w=w+kirim[i].berat;}
+    cout << "Total Berat angkutan yang di bawa adalah " << w << " kg"<<  endl;;
+}
+
+int main()
+{
+    int pilih;
+    int datake=1;
+    do
+    {
+        cout<<"MENU UTAMA" << endl;
+        cout<<"1. Tambah Data" << endl;
+        cout<<"2. Hapus Data Terakhir"<<endl;
+        cout<<"3. Tampil Data"<<endl;
+        cout<<"4. Total Harga"<<endl;
+        cout<<"5. Total Berat"<<endl;
+        cout<<"9. Keluar"<<endl;
+
+        cout<<"Pilihan Anda [1-9] : ";
+        cin>>pilih;
+        if(pilih==1)
+            { /*Menambah data baru diletakan di DATAKE*/
+                tambahdata(datake);
+                datake++;
+            }
+        if(pilih==2)
+            { /*Menghapus Data Terakhir (DATAKE-1*/
+                hapusdata(datake-1);
+                datake--;
+            }
+        if(pilih==3){ /*Menampilkan Data mulai
+                        data ke-1 hingga DATAKE*/
+            tampildata(datake);
+        }
+        if(pilih==4){ /*Menampilkan Total harga*/
+            totalHarga(datake);
+        }
+        if(pilih==5){ /*Menampilkan Total Angkutan*/
+        totalberat(datake);
+        }
+
+    }
+    while(pilih!=9);
+    return 0;
+}
